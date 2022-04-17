378b635b24ed79ef30465d605e54ab06fb979919
connor.mcshane@mail.com <connor.mcshane@mail.com>
2022-04-18 00:42:03 +0100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/connor-ps/gcs_lock_thing',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
