a36289e3b31dfdd893f4ca76c9a68028b408e056
connor.mcshane@mail.com <connor.mcshane@mail.com>
2022-04-18 12:53:53 +0100

    add unit tests for lock

---
+++ gcs_lock_thing/client.py
@@ -0,0 +1,107 @@
+"""
+Try and create lock based off algorithm described here
+https://www.joyfulbikeshedding.com/blog/2021-05-19-robust-distributed-locking-algorithm-based-on-google-cloud-storage.html
+"""
+from datetime import datetime, timedelta
+
+import backoff
+import logging
+import uuid
+
+import pytz
+
+logging.basicConfig(level=logging.debug)
+
+# Imports the Google Cloud client library
+from google.cloud import storage
+from google.api_core.exceptions import PreconditionFailed
+
+# Instantiates a client
+client = storage.Client()
+
+
+class LockClient:
+    client = storage.Client()
+
+    def __init__(self, bucket, lock_file_path: str = "gcs_lock_thing.txt", ttl=30, lock_id_prefix='default'):
+        self.bucket = bucket
+        self.lock_file_path = lock_file_path
+        self.ttl = ttl
+        self.bucket = client.get_bucket(bucket)
+        self.lock_file_path = lock_file_path
+        self.blob = self.bucket.blob(lock_file_path)
+        self.lock_id_prefix = lock_id_prefix
+
+    def lock(self):
+        """
+        Creates a lock with the specified GCS path.
+        :param lock_path: the lock's GCS path with the gs://bucket-name/file-name format
+        :return: boolean, if lock acquired or not
+        """
+
+        logging.info(f"Acquiring lock: {self.lock_file_path}")
+
+        try:
+            self._upload_lock_file()
+            logging.info("Lock acquired: {}".format(self.lock_file_path))
+            return True
+        except PreconditionFailed:  # this means lock already exists
+            logging.info(f"lock as its already in use, checking expiration: {self.lock_file_path}")
+            # check if lock is expired
+            blob_metadata = self.bucket.get_blob(self.lock_file_path).metadata
+            expiration_timestamp = datetime.fromisoformat(blob_metadata.get('expiration_timestamp'))
+
+            if expiration_timestamp < datetime.utcnow():  # lock is stale so we bin it
+                self.free_lock()
+                self.lock()
+                return True
+
+            logging.info("lock is not stale so we wait....")
+            return False
+
+    def free_lock(self):
+        """
+        Free lock
+        """
+        self.bucket.blob(self.lock_file_path).delete()
+        logging.info(f"Lock released: {self.lock_file_path}")
+        return True
+
+    def wait_for_lock(self, *backoff_args, **backoff_kwargs):
+        """
+
+        """
+
+        @backoff.on_predicate(*backoff_args, **backoff_kwargs)
+        def backoff_lock():
+            print(f"Backing off lock release: {self.lock_file_path}")
+            return self.lock()
+
+        return backoff_lock()
+
+    def wait_for_lock_expo(self, base=2, factor=0.5, max_value=10, max_time=60, jitter=backoff.full_jitter, *args,
+                           **kwargs):
+        """
+        A helper function for `wait_for_lock` that uses exponential backoff.
+        :param base: waiting time (sec) is: factor * (base ** n)
+        :param factor: waiting time (sec) is: factor * (base ** n)
+        :param max_value: the ceiling value for retry time, in seconds
+        :param max_time: total retry timeout, in seconds
+        :param jitter: See backoff.on_predicate for details. Pass jitter=None for no jitter.
+        :return: If the lock was acquired or not
+        """
+        return self.wait_for_lock(wait_gen=backoff.expo, base=base, factor=factor, max_value=max_value,
+                                  max_time=max_time, jitter=jitter, *args, **kwargs)
+
+    def _upload_lock_file(self):
+        """
+        Upload dummy lock file with id and ttl metadata
+        """
+        file = 'lock.txt'
+        open(file, 'a').close()
+        self.blob.upload_from_filename(file, if_generation_match=0)
+        metadata = {'expiration_timestamp': datetime.utcnow() + timedelta(seconds=self.ttl),
+                    'lock_id': f"{self.lock_id_prefix}-{uuid.uuid4()}"
+                    }
+        self.blob.metadata = metadata
+        self.blob.patch()
--- tests/test_gcs_lock_thing.py
+++ tests/test_gcs_lock_thing.py
@@ -1,21 +1,52 @@
 #!/usr/bin/env python
 
 """Tests for `gcs_lock_thing` package."""
-
-
 import unittest
 
-from gcs_lock_thing import gcs_lock_thing
+from gcs_lock_thing.client import LockClient
+from google.cloud import storage
 
 
-class TestGcs_lock_thing(unittest.TestCase):
+class TestgcsLockThing(unittest.TestCase):
     """Tests for `gcs_lock_thing` package."""
+    public_bucket_path = "data-trf-test-mutex-lock"
+    lock_file_name = "test-lock.txt"
+    ttl = 2
+    client = LockClient(bucket=public_bucket_path, lock_file_path=lock_file_name, ttl=ttl)
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(self.public_bucket_path)
+        blobs = bucket.list_blobs(prefix='test-lock.txt')
+        for blob in blobs:
+            blob.delete()
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(self.public_bucket_path)
+        blobs = bucket.list_blobs(prefix='test-lock.txt')
+        for blob in blobs:
+            blob.delete()
 
-    def test_000_something(self):
-        """Test something."""
+    def test_basic_lock(self):
+        """Test basic use"""
+        lock_acquired_status = self.client.lock()
+        self.assertTrue(lock_acquired_status)
+        free_lock_status = self.client.free_lock()
+        self.assertTrue(free_lock_status)
+
+    def test_lock_not_free(self):
+        """
+        test lock not free
+        """
+        lock_acquired_status = self.client.lock()
+        lock_acquired_status_second_attempt = self.client.lock()
+        self.assertTrue(lock_acquired_status)
+        self.assertFalse(lock_acquired_status_second_attempt)
+
+    def test_wait_lock_to_free(self):
+        self.client.lock()
+        lock_acquired_status_second_attempt = self.client.wait_for_lock_expo()
+        self.assertTrue(lock_acquired_status_second_attempt)
